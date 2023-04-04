328a01b0d2f027d8fdc9ce1f6e07eccf944e4855
Zaki <Zaki@Ahmads-Air-2.attlocal.net>
2023-04-03 20:34:59 -0500

    Adding main.js and modified index.html

---
+++ main.js
@@ -0,0 +1,10 @@
+// IIFE
+
+(function () {
+  // Targeting
+
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector("btn-clear");
+  let equal = doucment.querySelector(".btn-equal");
+})();
