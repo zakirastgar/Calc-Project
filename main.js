cdadadab5fdde02251cf3f0200fbc36ba701662f
Zaki <Zaki@Ahmads-Air-2.attlocal.net>
2023-04-04 19:48:35 -0500

    Added Functionality to display user clicked input on our calculator

--- main.js
+++ main.js
@@ -7,4 +7,14 @@
   let buttons = document.querySelectorAll(".btn");
   let clear = document.querySelector("btn-clear");
   let equal = doucment.querySelector(".btn-equal");
+
+  // first Functionality
+
+  // buttons.forEach(function())
+  buttons.forEach(fuction(button){
+    button.addEventListern('click', function(e) {
+      let userInput = e.target.dataset.num;
+      screen.value += userInput;
+    })
+  })
 })();
